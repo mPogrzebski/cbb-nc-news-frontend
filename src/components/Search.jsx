43955f86e7310f73b858bf134060b72d7bae895d
Mateusz Pogrzebski <mateusz.pogrzebski@gmail.com>
2021-10-21 09:39:04 +0100

    Removed tailwindcss

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,20 @@
-import React from "react";
-import {  useLocation, useParams } from "react-router";
+import React, { useState, useEffect } from "react";
+import {  useLocation, useParams } from "react-router-dom";
 
 export default function Search(props) {
 
     const search = useLocation().search;
 
-    const searchTerm = new URLSearchParams(search).get("searchWord");
+    const [searchTerm, setSearchTerm] = useState('');
 
+    const searchWord = new URLSearchParams(search).get("searchWord");
+
+    useEffect(() => {
+      setSearchTerm(searchWord);
+      
+      
+
+    }, [searchWord])
 
   return (
     <div>
